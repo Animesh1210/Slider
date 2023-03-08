5819208f9719644bbff57a37d06b12b9759f3aae
Animesh <animeshsingh.mail@gmail.com>
2023-03-08 18:06:55 +0530

    slider transition done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("in js")
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const slider = document.querySelector('.slider');
+const images =document.querySelectorAll('.image');
+let pixel=1;
+console.log(images.length);
+right.addEventListener('click',()=>{
+    if(pixel<images.length){  
+    slider.style.transform = `translateX(-${800*pixel}px)`; 
+    pixel++;
+    }
+    else{
+        slider.style.transform = `translateX(0px)`; 
+        pixel=1;
+
+    }
+    
+});
+
