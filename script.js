1022cb8f466ced75059ae59f0b179a4a106b4f50
Animesh <animeshsingh.mail@gmail.com>
2023-03-08 19:44:39 +0530

    Scroll working

--- script.js
+++ script.js
@@ -3,15 +3,25 @@ const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
 const images =document.querySelectorAll('.image');
 let pixel=1;
-console.log(images.length);
+const length=images.length;
 right.addEventListener('click',()=>{
-    if(pixel<images.length){  
+    if(pixel<length){  
     slider.style.transform = `translateX(-${800*pixel}px)`; 
     pixel++;
     }
     else{
         slider.style.transform = `translateX(0px)`; 
         pixel=1;
+    }
+    });
+left.addEventListener('click',()=>{
+    if(pixel!=1){  
+    slider.style.transform = `translateX(-${800*(pixel-2)}px)`; 
+    pixel--;
+    }
+    else{
+        slider.style.transform = `translateX(-${800*(length-1)}px)`; 
+        pixel=(length);
 
     }
     
